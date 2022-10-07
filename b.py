c1760d3d50df0a6b58ef95e50e80e7727b4330f2
gshsgdjsb41846 <113258450+gshsgdjsb41846@users.noreply.github.com>
2022-10-07 19:41:04 +0600

    Update b.py

--- b.py
+++ b.py
@@ -7,7 +7,7 @@ def stt(u,c):
     u.message.reply_text("Choltase bhy bot")
 
 
-upt = Updater("5666523095:AAEAIjO1IvbllN2hIHVB4aG5OCDvNZe0vm8", use_text=True)
+upt = Updater("5666523095:AAEAIjO1IvbllN2hIHVB4aG5OCDvNZe0vm8", use_context=True)
 
 upt.dispatcher.add_handler(CommandHandler("start",stm))
 upt.dispatcher.add_handler(CommandHandler("status",stt))
