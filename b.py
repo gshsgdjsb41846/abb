74b560db4621cb1f9ccbb289af551d66c16242a3
gshsgdjsb41846 <113258450+gshsgdjsb41846@users.noreply.github.com>
2022-10-07 19:34:56 +0600

    Create b.py

---
+++ b.py
@@ -0,0 +1,16 @@
+from telegram.ext import *
+
+def stm(u,c):
+    u.message.reply_text("Bruhh")
+
+def stt(u,c):
+    u.message.reply_text("Choltase bhy bot")
+
+
+upt = Updater("5666523095:AAEAIjO1IvbllN2hIHVB4aG5OCDvNZe0vm8", use_text=True)
+
+upt.dispatcher.add_handler(CommandHandler("start",stm))
+upt.dispatcher.add_handler(CommandHandler("status",stt))
+
+upt.start()
+idle()
